a81de6c4b86dcae738b1447c57f893c8da7c59cd
Sujal Shinde <sujalshinde200411@gmail.com>
2026-07-23 18:56:51 +0530

    Add Data Types

---
+++ 1. Variables, Data Types and print Function/2. Data Types.py
@@ -0,0 +1,12 @@
+Name = "Sujal"
+age = 20
+gender = "Male"
+is_student = True
+marks = 26.6
+
+print(type(Name))
+print(type(age))
+print(type(gender))
+print(type(is_student))
+print(type(marks))
+
