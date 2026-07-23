0836a99a7aa3f323f5627c331f8e4aa9d1b2b880
Sujal Shinde <sujalshinde200411@gmail.com>
2026-07-23 20:15:36 +0530

    Add Practice Question

---
+++ 1. Variables, Data Types and print Function/practice.py
@@ -0,0 +1,12 @@
+"""
+Q1: Take two numbers as input from the user. Print their sum, difference,
+product, and remainder.
+"""
+
+num_1 = int(input("Enter your First Number : "))
+num_2 = int(input("Enter your Second Number : "))
+
+print(f"The Sum of the Numbers are : {num_1+num_2}")
+print(f"The Difference of the Numbers are : {num_1-num_2}")
+print(f"The Product of the Numbers are : {num_1*num_2}")
+print(f"The Remainder of the Numbers are : {num_1%num_2}")
