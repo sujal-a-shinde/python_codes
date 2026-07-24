bc7390d67dc9a3fee7ce997ad244c9d199c05a41
Sujal Shinde <sujalshinde200411@gmail.com>
2026-07-24 21:39:37 +0530

    Add Escape sequence

---
+++ 2. Escape Sequences and Operators/4. Escape sequence.py
@@ -0,0 +1,17 @@
+"""
+\n - New Line
+\t - Tab Space
+\\ - Backslash
+\" - Double quotes
+\' - Single quotes
+"""
+
+# print("He\nllo\nWorl\nd")
+# print("Hello\tWorld")
+
+# S\\\ujal
+# print("S\\\\\\ujal")
+
+# My name is "Sujal" Shinde
+# print('My name is "Sujal" Shinde')
+# print('My name is "Anirudh" Khurana')
