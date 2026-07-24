66e4edd5f576921eee295aad802ae856961e79f4
Sujal Shinde <sujalshinde200411@gmail.com>
2026-07-24 21:38:38 +0530

    Add Logical Operators

---
+++ 2. Escape Sequences and Operators/3. Logical_Operators.py
@@ -0,0 +1,15 @@
+# AND, OR, NOT
+
+English = 25
+Maths = 23
+
+# # Print True if pass in both Subject
+# print(English >= 25 and Maths >= 20)
+
+# # Print True if pass in any one Subject
+# print(English > 25 or Maths < 20) # False
+# print(English >= 25 or Maths < 20) # True
+
+# NOT
+# print(not English > 25 and not Maths < 20)
+# print(not (English > 25 and Maths < 20))
