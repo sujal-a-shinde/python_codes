848c920452c8d6791dcfb21c83941b21bce37cb4
Sujal Shinde <sujalshinde200411@gmail.com>
2026-07-24 21:34:28 +0530

    Add Arithmatic Operator

---
+++ 2. Escape Sequences and Operators/1. Arithmatic_Operator.py
@@ -0,0 +1,20 @@
+a = 15
+b = 10
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+
+
+# Floor Division (it always give answer in (Int))
+
+print(a // b)
+
+# Exponent
+
+print(5**2)  # used for power that 5x5
+print(2**0.5)  # for square root
+
+# Modulus (Remainder)
+print(21 % 10)
