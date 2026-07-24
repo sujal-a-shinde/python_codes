a8efd6ebd79118ccf3a5eca2c0466daf6e69ab44
Sujal Shinde <sujalshinde200411@gmail.com>
2026-07-24 21:37:11 +0530

    Add Comparision Or Relational Operator

---
+++ 2. Escape Sequences and Operators/2. Comparision_or_Relational Operator.py
@@ -0,0 +1,11 @@
+a = 2
+b = 4
+
+print(a == b)  # False
+print(a >= b)  # False
+print(a <= b)  # True (becoz it is smaller then b)(it checks both operator)
+print(a > b)   # False
+print(a < b)   # True
+print(a != b)  # True
+
+# The Output will always in "Boolean" i.e True or False 
