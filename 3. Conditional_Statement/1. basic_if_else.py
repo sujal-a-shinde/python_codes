94068d07b78f43dc2ea8e9294de086c518ac766d
Sujal Shinde <sujalshinde200411@gmail.com>
2026-07-25 11:43:10 +0530

    Add Basic if-else

---
+++ 3. Conditional_Statement/1. basic_if_else.py
@@ -0,0 +1,16 @@
+# age = int(input("Enter your Age : "))
+
+# if age >= 18:
+#     print("You can vote")
+#     print("You are eligible")
+#     print("Your responsible voter")
+# else:
+#     print("You can't_vote")
+
+Maths = int(input("Enter your Math_Marks : "))
+Chemistry = int(input("Enter your Chemistry_Marks : "))
+
+if Maths >= 35 and Chemistry >= 35:
+    print("Pass")
+else:
+    print("Fail")
