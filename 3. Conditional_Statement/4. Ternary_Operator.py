8348b1a7ffdf5afede1cd3f4736c6a1f4bb0fea5
Sujal Shinde <sujalshinde200411@gmail.com>
2026-07-26 11:02:35 +0530

    Add Learn Ternary Operator

---
+++ 3. Conditional_Statement/4. Ternary_Operator.py
@@ -0,0 +1,11 @@
+age = int(input("Rnter your age : "))
+
+# if age >= 18:
+#     status = "Adult"
+# else:
+#     status = "Minor"
+
+# print(f"Your status is {status}")
+
+status = "adult" if age >= 18 else "minor"
+print(status)
