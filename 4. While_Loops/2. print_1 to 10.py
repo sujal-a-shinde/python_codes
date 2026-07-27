e3c5e915000c5c8cf0dc855bdf2adc81c1ac66c8
Sujal Shinde <sujalshinde200411@gmail.com>
2026-07-27 09:59:13 +0530

    Add Printed 1 to 10

---
+++ 4. While_Loops/2. print_1 to 10.py
@@ -0,0 +1,15 @@
+# print_1 to 10
+
+# i = 1
+# while i <= 10:
+#     print(i, end=" ")
+#     i += 1
+
+# print 1 to n , n is the number input by the user
+
+n = int(input("Enter Your Number : "))
+
+i = 1
+while i <= n:
+    print(i, end=" ")
+    i += 1
