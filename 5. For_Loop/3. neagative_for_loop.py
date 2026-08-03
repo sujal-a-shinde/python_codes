91e5c4c4c62b645b42c18c8c444c9c80a1acf86e
Sujal Shinde <sujalshinde200411@gmail.com>
2026-08-03 11:20:51 +0530

    Add Neagative for Loop

---
+++ 5. For_Loop/3. neagative_for_loop.py
@@ -0,0 +1,18 @@
+# 10 to 1
+
+for i in range(10, 0, -1):
+    print(i, end=" ")
+
+# vice versa
+
+for i in range(1, 11, -1):
+    print(i, end=" ")
+
+# left side
+for i in range(1, -11, -1):
+    print(i, end=" ")
+
+# conditional statement
+for i in range(100, 0, -1):
+    if i % 2 == 0 and i % 3 == 0:
+        print(i, end=" ")
