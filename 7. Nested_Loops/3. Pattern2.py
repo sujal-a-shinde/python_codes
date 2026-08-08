65113470c90753147125592e3d2766cff2de2e31
Sujal Shinde <sujalshinde200411@gmail.com>
2026-08-08 22:21:44 +0530

    Add Pattern 2

---
+++ 7. Nested_Loops/3. Pattern2.py
@@ -0,0 +1,26 @@
+"""
+1 1 1 1 1
+2 2 2 2 2
+3 3 3 3 3
+4 4 4 4 4
+5 5 5 5 5
+"""
+
+for i in range(1, 6):
+    for j in range(1, 6):
+        print(i, end=" ")
+    print()
+
+
+"""
+5 5 5 5 5
+4 4 4 4 4
+3 3 3 3 3
+2 2 2 2 2
+1 1 1 1 1
+"""
+
+for i in range(5, 0, -1):
+    for j in range(1, 6):
+        print(i, end=" ")
+    print()
