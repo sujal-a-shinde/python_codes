410e034f8d4f474a2decd78f9d747f16d84d588e
Sujal Shinde <sujalshinde200411@gmail.com>
2026-08-12 18:13:26 +0530

    Add Pattern 4

---
+++ 7. Nested_Loops/5. pattern4.py
@@ -0,0 +1,25 @@
+"""
+1
+1 2
+1 2 3
+1 2 3 4
+1 2 3 4 5
+"""
+
+for i in range(1, 6):
+    for j in range(1, i + 1):
+        print(j, end=" ")
+    print()
+
+
+"""
+1 2 3 4 5
+1 2 3 4
+1 2 3
+1 2
+1
+"""
+for i in range(5, 0, -1):
+    for j in range(1, i + 1):
+        print(j, end=" ")
+    print()
