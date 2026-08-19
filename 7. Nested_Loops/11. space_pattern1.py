ad0c4a50d2c357586b38d790f54c96bfcb56868b
Sujal Shinde <sujalshinde200411@gmail.com>
2026-08-19 21:29:43 +0530

    Add Space Pattern 1

---
+++ 7. Nested_Loops/11. space_pattern1.py
@@ -0,0 +1,29 @@
+"""
+        1
+      1 2
+    1 2 3
+  1 2 3 4
+1 2 3 4 5
+"""
+
+for i in range(1, 6):
+    for k in range(1, 6 - i):
+        print(" ", end=" ")
+    for j in range(1, i + 1):
+        print(j, end=" ")
+    print()
+
+
+"""
+1 2 3 4 5
+  1 2 3 4
+    1 2 3
+      1 2
+        1
+"""
+for i in range(5, 0, -1):
+    for k in range(1, 6 - i):
+        print(" ", end=" ")
+    for j in range(1, i + 1):
+        print(j, end=" ")
+    print()
