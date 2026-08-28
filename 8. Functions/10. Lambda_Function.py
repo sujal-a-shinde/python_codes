9a360532f4b35aa718b8c1464a574421b6fcf6ba
Sujal Shinde <sujalshinde200411@gmail.com>
2026-08-28 22:43:12 +0530

    Learn Lambda Function

---
+++ 8. Functions/10. Lambda_Function.py
@@ -0,0 +1,16 @@
+square = lambda num: num * num
+
+print(square(2))
+
+
+# Reture True or False if age >=18
+
+is_age = lambda age: True if age >= 18 else False
+
+print(is_age(23))
+
+#
+
+is_age = lambda age: age >= 18
+
+print(is_age(23))
