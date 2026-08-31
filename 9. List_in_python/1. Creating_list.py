98a3e59b453c05b8dc372204a8e7d25b850ccd54
Sujal Shinde <sujalshinde200411@gmail.com>
2026-08-31 21:30:04 +0530

    Learn to Creating list

---
+++ 9. List_in_python/1. Creating_list.py
@@ -0,0 +1,16 @@
+# Creating a list
+
+names = ["Sujal", "Anuj", "Zaid", True, 44, 34.111]
+print(names)
+print(type(names))
+
+
+# List operation : +,-,*,/
+
+marks = [23, 4, 33, 12, 5]
+print(marks * 4)  # (here only * will apply other will give error) when single list * number 
+
+marks = [23, 4, 33, 12, 5]
+marks2 = [1, 3, 45, 66, 7]
+print(marks + marks2) # (here only + will apply other will give error) when list + list
+
