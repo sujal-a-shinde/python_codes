2dccaebeb083defa19863029e355f956359605be
Sujal Shinde <sujalshinde200411@gmail.com>
2026-09-01 23:46:55 +0530

    Learn Indexing in List

---
+++ 9. List_in_python/3. Indexing.py
@@ -0,0 +1,13 @@
+lst = ["Sujal", "Anuj", 56.12, 91, 100, "Pune"]
+
+print(lst[0])
+print(lst[-1])
+print(lst[-5])
+print(lst[2])
+
+
+# print last index value without -1
+
+n = len(lst)
+print(f"Last index value : {lst[n-1]}")
+print(n)
