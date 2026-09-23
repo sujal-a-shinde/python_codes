e3eb661476e0f088af742baa958c67dd217f736a
Sujal Shinde <sujalshinde200411@gmail.com>
2026-09-23 11:05:24 +0530

    Add Basic of Tuple

---
+++ 10. Tuples_in_python/1. Basic.py
@@ -0,0 +1,13 @@
+my_tuple = (1, 2, 3, "Sujal")
+
+print(my_tuple)
+print(type(my_tuple))
+
+
+# Immutable
+# my_tuple[0]= 100 # error
+
+print(my_tuple.count(1))
+print(my_tuple.index("Sujal"))
+print(my_tuple)
+
